93bca7a43df2566508464c5420c9d800d7a60f1a
Kyle Luo <kyleluo@andrew.cmu.edu>
2026-06-30 14:56:51 -0400

    Ensure I comitted

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1s%660rbrzw=n&n3x4z*0xz^bup)03%+#grtpdg2u3a@^h=xpw'
+
 
 # --- Secret key & debug ---
 SECRET_KEY = config('SECRET_KEY')
